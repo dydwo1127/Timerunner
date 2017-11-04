bf92eb39ea55ac6fbd5b9773fe796e1db785630f
Song <dydwo1127@gmail.com>
2017-11-04 17:01:16 +0900

    장애물 랜덤생성 테스트

---
+++ Assets/ObsMaker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsMaker : MonoBehaviour {
+
+    public List<GameObject> Obstacles;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Instantiate(Obstacles[Random.Range(0, 5)]).transform.position = new Vector3(0,0,0);
+        }
+	}
+}
