e01d2f6ac7170c858fbe1c1ded4c778e69f5557f
user-PC\user <dydwo1127@gmail.com>
2017-11-18 16:50:01 +0900

    죽...여...줘.......

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
     public int stageTime;
-    
+    public float backgroundSpeed;
+
+    public GameObject background;
+
+    Vector2 backgroundOffset;
+
 	void Start ()
     {
-        
+        backgroundOffset = background.GetComponent<SpriteRenderer>().material.mainTextureOffset;
 	}
 	
 	void Update ()
     {
+        backgroundOffset.x = backgroundSpeed * Time.deltaTime;
 		if(Time.time > stageTime)
         {
             GetComponent<ObsMaker>().isTimeRunning = false;
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float jumpSpeed;
+    public BoxCollider2D standCollider;
+    public BoxCollider2D slideCollider;
 
     Animator anim;
 
     int jumpCount = 0;
 	void Start () {
         anim = GetComponent<Animator>();
+        standCollider.enabled = true;
+        slideCollider.enabled = false;
+
 	}
 	
 	void Update ()
@@ -32,12 +37,42 @@ public class Player : MonoBehaviour {
             
         }
 
-        if(Input.GetKeyDown(KeyCode.Space) && IsOnFloor() && jumpCount <2)
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Slide();
+        }
+        if(Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            SlideUp();
+        }
+	}
+
+    public void Jump()
+    {
+        if (IsOnFloor() && jumpCount < 2)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector3(0, jumpSpeed, 0);
             jumpCount++;
         }
-	}
+    }
+
+    public void Slide()
+    {
+        anim.SetTrigger("slide");
+        standCollider.enabled = false;
+        slideCollider.enabled = true;
+    }
+
+    public void SlideUp()
+    {
+        anim.SetTrigger("slideUp");
+        standCollider.enabled = true;
+        slideCollider.enabled = false;
+    }
 
     bool IsOnFloor()
     {
